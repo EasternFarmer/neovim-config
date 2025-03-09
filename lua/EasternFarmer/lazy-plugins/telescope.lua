76aed391c589ffc7cbb4e8364497fbf496315465
ItsRedCropie <franek.banas1@gmail.com>
2025-03-09 21:38:40 +0100

    update 1

---
+++ lua/EasternFarmer/lazy-plugins/telescope.lua
@@ -0,0 +1,20 @@
+return {
+    "nvim-telescope/telescope.nvim",
+
+    tag = "0.1.5",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+        vim.keymap.set('n', '<C-g>', builtin.git_files, {})
+        vim.keymap.set('n', '<C-S-f>', function()
+            builtin.find_files { cwd = vim.fn.stdpath 'config' }
+        end, { desc = '[S]earch [N]eovim files' })
+    end,
+}
